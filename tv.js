bc74b572e2f7ec7950011447871c1f7205d8335d
Antony Hernandez <wessoman@hotmail.com>
2019-06-26 13:53:32 -0500

    New and Improved TV APPgit add .! YAAAY BONUS SIZE

--- tv.js
+++ tv.js
@@ -1,9 +1,26 @@
+var axios = require('axios');
+
 var TV = function() {
   this.findShow = function(show) {
     // The following URL can be used to search the TV Maze API for a given show
-    var URL = "http://api.tvmaze.com/singlesearch/shows?q=" + show;
+    var URL = "https://api.tvmaze.com/singlesearch/shows?q=" + show;
+    // var URL = "http://api.tvmaze.com/singlesearch/shows?q=breaking+bad";
 
+axios.get(URL).then(function(response) {
+    
+    console.log(`
+    Show Name: ${response.data.name}
+    Genres: ${response.data.genres.join(" ")}
+    Rating: ${response.data.rating.average}
+    Network: ${response.data.network.name}
+    Summary: ${response.data.summary}
+    `)
+})
   };
 };
+
+var tv=new TV();
+tv.findShow('breaking bad');
+
 
 module.exports = TV;
