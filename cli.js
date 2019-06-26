a7dd36817217a670d2059231a4feccc3e4549239
elaintran <elain.ttran@gmail.com>
2019-06-26 14:33:11 -0500

    connected cli and tv

--- cli.js
+++ cli.js
@@ -1,21 +1,25 @@
+var TV = require("./TV");
+
 var consoleString = ''
 
 function makeString () {
 
  for(i=3; i<=process.argv.length-1; i++) {
    consoleString += process.argv[i]+ ' ' ;
-   console.log(process.argv[i]);
+   //console.log(process.argv[i]);
  }
 
- console.log("string:" + consoleString);
+ //console.log("string:" + consoleString);
 }
 
 function checkArgs () {
  if(process.argv[2] == 'show' ) {
-   console.log("show...")
+   //console.log("show...")
    makeString();
+   var tv = new TV();
+   tv.findShow(consoleString);
  } else if (process.argv[2] == 'actor') {
-   console.log("show...");
+   //console.log("show...");
    makeString();
  }
 }
